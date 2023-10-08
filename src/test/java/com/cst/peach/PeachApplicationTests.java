c1afaa53d87dc109b7c4c54f374648ddc76dbca9
umanose37 <myoko.brand.coffee@gmail.com>
2023-10-08 22:58:51 +0900

    add test class

--- src/test/java/com/cst/peach/PeachApplicationTests.java
+++ src/test/java/com/cst/peach/PeachApplicationTests.java
@@ -3,11 +3,26 @@ package com.cst.peach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootTest
 class PeachApplicationTests {
 
 	@Test
 	void contextLoads() {
+
+	}
+
+	void main() {
+		Map<String, String> map = new HashMap<>();
+		map.put("color", "red");
+		map.put("size", "XL");
+
+		System.out.println(map.toString());
+		if (map.containsKey("qty")) System.out.println("qty");
+		if (!map.containsKey("qty")) System.out.println("not qty");
+
 	}
 
 }
